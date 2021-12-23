6b94fdd2a43cf4bd337f40149ccf7d08e2dfc003
Mitchell Reid <mitchellreid02@gmail.com>
2021-12-23 12:42:46 +1100

    Remove colours

--- staticProps/wines.tsx
+++ staticProps/wines.tsx
@@ -9,19 +9,19 @@ const wines: Array<Wine> = [
   {
     codeName: "Wine 1",
     name: "Four Corners",
-    colour: "bg-green-500",
+    colour: "bg-white",
     value: 10,
   },
   {
     codeName: "Wine 2",
     name: "Bottom Shelf",
-    colour: "bg-red-500",
+    colour: "bg-white",
     value: 100,
   },
   {
     codeName: "Wine 3",
     name: "Literally $1000",
-    colour: "bg-yellow-500",
+    colour: "bg-white",
     value: 1000,
   },
 ].sort((a, b) => b.value - a.value);
