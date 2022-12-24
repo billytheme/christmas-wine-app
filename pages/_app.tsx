a255f35191e731e2c5097562a352983f25218e4c
Mitchell Reid <mitchellreid02@gmail.com>
2022-12-24 15:35:43 +1100

    Add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Reid Blind Tasting</title>
+      </Head>
       <div className="flex justify-center p-8 bg-red-500 w-full">
         <p className="text-3xl font-bold text-green-500 text-center">
           Reid Blind Tasting
