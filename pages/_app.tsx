867d6bcc06cab971f1291763840c068a9f30d3f4
Mitchell Reid <mitchellreid02@gmail.com>
2022-12-24 15:35:02 +1100

    Further remove wine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     <SessionProvider session={session}>
       <div className="flex justify-center p-8 bg-red-500 w-full">
         <p className="text-3xl font-bold text-green-500 text-center">
-          Reid Wine Tasting
+          Reid Blind Tasting
         </p>
       </div>
       <div className="p-4">
